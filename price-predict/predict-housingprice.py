4b77d03bcdddd6e0da91d7c6711d25bb93debf25
Joel Benjamin <joelbenjamin093@gmail.com>
2017-03-28 20:24:28 +0530

    Add files via upload

--- price-predict/predict-housingprice.py
+++ price-predict/predict-housingprice.py
@@ -27,36 +27,29 @@ def LoadCSVData(filename):
 	with open(filename,'r') as data:
 		csvData = csv.reader(data)
 		for x in csvData:
-			SizeInft.append(x[5])
-			HousePrice.append(x[2])
+			SizeInft.append(float(x[5]))
+			HousePrice.append(float(x[2]))
 		print("Data Loaded from {0}".format(filename))
 	return [SizeInft,HousePrice]
 
-def SplitToTrainAndTest(FtrainingListSize,FtestingListSize,FtrainingListPrice,FtestingListPrice,REdata,splitRatio = 0.63):
+def SplitToTrainAndTest(FtrainingListSize,FtestingListSize,FtrainingListPrice,FtestingListPrice,REdata,splitRatio = 0.63,Ftype = 'eq'):
 	# the prefix 'F' is added to the function varialbles just to avoid confusion, because without them they have the same
 	# names as the lists
-	if len(REdata[0]) == len(REdata[1]):
-		# split House size data into training and testing data
-		for x in range(len(REdata[0])):
-			if random.random() < splitRatio:
-				FtrainingListSize.append(float(REdata[0][x]))
-			else:
-				FtestingListSize.append(float(REdata[1][x]))
-		print("Splited House size data into T&T")
-		# split price data into training and testing data
-		for x in range(len(REdata[1])):
-			if random.random() < splitRatio:
-				FtrainingListPrice.append(float(REdata[0][x]))
-			else:
-				FtestingListPrice.append(float(REdata[1][x]))
-		print("Splited House price data into T&T")
-
-#def AdjustDataToEqual(x,y):
-	# To plot the data on the scatter plot they should be equal
-#	gap = max(x,y) - min(x,y)
-#	for x in range(gap):
-#		y.append(0)
-#	print(x,y)
+		if len(REdata[0]) == len(REdata[1]):
+			# split House size data into training and testing data
+			for x in range(len(REdata[0])):
+				if random.random() < splitRatio:
+					FtrainingListSize.append(float(REdata[0][x]))
+				else:
+					FtestingListSize.append(float(REdata[1][x]))
+			print("Splited House size data into T&T")
+			# split price data into training and testing data
+			for x in range(len(REdata[1])):
+				if random.random() < splitRatio:
+					FtrainingListPrice.append(float(REdata[0][x]))
+				else:
+					FtestingListPrice.append(float(REdata[1][x]))
+			print("Splited House price data into T&T")
 
 
 def main():
@@ -73,11 +66,11 @@ def main():
 	print("SizeHouse Testing Data : {0}".format(len(testingListSize)))
 	print("Price Training Data: {0}".format(len(trainingListPrice)))
 	print("Price Testing Data : {0}".format(len(testingListPrice)))
-	print(len(xsTrain))
-	print(len(ysTrain))
-	AdjustDataToEqual(len(xsTrain),len(ysTrain))
-	#plt.scatter(xsTrain,ysTrain)
-	#plt.show()
+	#print(len(xsTrain))
+	#print(len(ysTrain))
+	#adjVal = AdjustDataToEqual(len(xsTrain),len(ysTrain),xsTrain,ysTrain)
+	plt.scatter(CSVFileData[0],CSVFileData[1])
+	plt.show()
 
 
 if __name__ == '__main__':
